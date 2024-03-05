e12773051071f3c2435a01bab9ef853704f67146
Ayush Mathur <gaurav.vaishnav@atcs.com>
2024-03-05 11:39:24 +0530

    --> changes

--- src/main/java/com/example/demo/controller/AccountController.java
+++ src/main/java/com/example/demo/controller/AccountController.java
@@ -1,33 +1,25 @@
 package com.example.demo.controller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.example.demo.entity.Account;
-import com.example.demo.service.AccountService;
 
 @RestController
 @RequestMapping("/demo")
 public class AccountController {
 
-	@Autowired
-	AccountService accountService;
+//	@Autowired
+//	AccountService accountService;
 	
-	@PostMapping("/setAccount")
-	public Account setAccount(@RequestBody Account account) {
-		return accountService.setAccount(account);
-	}
-	
-	@GetMapping("/getAccounts")
-	public List<Account> getAccounts(){
-		return accountService.getAccounts();
-	}
+//	@PostMapping("/setAccount")
+//	public Account setAccount(@RequestBody Account account) {
+//		return accountService.setAccount(account);
+//	}
+//	
+//	@GetMapping("/getAccounts")
+//	public List<Account> getAccounts(){
+//		return accountService.getAccounts();
+//	}
 
 	@GetMapping("/msg")
 	public String getMsg(){
--- src/main/java/com/example/demo/entity/Account.java
+++ src/main/java/com/example/demo/entity/Account.java
@@ -1,29 +1,29 @@
-package com.example.demo.entity;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Entity
-@Table
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-public class Account {
-
-	@Id
-	@Column(name = "id")
-	String id;
-	
-	@Column(name = "name")
-	String name;
-	
-	@Column(name = "phone")
-	String phone;
-}
+//package com.example.demo.entity;
+//
+//import jakarta.persistence.Column;
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.Id;
+//import jakarta.persistence.Table;
+//import lombok.AllArgsConstructor;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//import lombok.Setter;
+//
+//@Entity
+//@Table
+//@Getter
+//@Setter
+//@AllArgsConstructor
+//@NoArgsConstructor
+//public class Account {
+//
+//	@Id
+//	@Column(name = "id")
+//	String id;
+//	
+//	@Column(name = "name")
+//	String name;
+//	
+//	@Column(name = "phone")
+//	String phone;
+//}
